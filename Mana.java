9b3277f8964471d31de8ebcd5bcef1ccb6f64dd7
Auskip <57517994+Applethings@users.noreply.github.com>
2023-03-25 22:49:36 -0700

    Create Mana.java

---
+++ Mana.java
@@ -0,0 +1,34 @@
+package ga.pmc.auskip;
+
+public class Mana {
+    private int value;
+
+    public Mana(int value) {
+        this.value = value;
+    }
+
+    public Mana plus(Mana other) {
+        return new Mana(this.value + other.value);
+    }
+
+    public boolean isLessThan(Mana other) {
+        return this.value < other.value;
+    }
+
+    public boolean isGreaterThan(Mana other) {
+        return this.value > other.value;
+    }
+
+    @Override
+    public String toString() {
+        return String.valueOf(value);
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+}
